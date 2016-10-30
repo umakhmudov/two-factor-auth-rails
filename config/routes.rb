6641d096a22cdcbad62e64f94b2bce527f4193ea
Ulugbek Makhmudov <ulugbek_u@hotmail.com>
2016-10-30 22:09:19 +0400

    added session controller to manage sessions and login/logout actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
 
-  # sign up page route
+  # sign up page
   get '/signup' => 'users#new'
 
-  # user creation route
+  # user creation (signup)
   post '/users' => 'users#create'
+
+  #login page
+  get '/login' => 'sessions#new'
+
+  #session creation(login)
+  post '/login' => 'sessions#create'
+
+  #session destroy (logout)
+  get '/logout' => 'sessions#destroy'
 
 end
